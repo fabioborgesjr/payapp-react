a8e5c23caf519334be24a19aa0ddeb5c5ea6c1d1
salazenas <salazenas@gmail.com>
2019-07-23 00:19:40 -0300

    header, sidebar, menu and footer definitions

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import '../common/template/dependecies'
+import Header from '../common/components/header/header';
+import Sidebar from '../common/components/sidebar/sidebar';
+import Footer from '../common/components/footer/footer';
 
 export default props => (
     <div className='wrapper'>
-        <h1>Payapp</h1>
+        <Header />
+        <Sidebar />
+        <div className="content-wrapper">
+            <h1>Sample content</h1>
+        </div>
+        <Footer />
     </div>
 )
