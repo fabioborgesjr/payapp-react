e7d215c2791f8e3861a1fd4462dc075695c68c00
salazenas <salazenas@gmail.com>
2019-07-25 13:29:12 -0300

    initial redux store

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,7 +1,17 @@
 import ReactDOM from 'react-dom'
 import React from 'react'
+
+import { createStore } from 'redux';
+import { Provider } from 'react-redux'
+
+import reducers from './main/reducers';
+
 import App from './main/app';
 
+const store = createStore(reducers)
+
 ReactDOM.render(
-    <App />
-, document.getElementById('app'))
+    <Provider store={store}>
+        <App />
+    </Provider>
+    , document.getElementById('app'))
