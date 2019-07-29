3fddcd6556aacf08975db641de38b79bce8a6fea
salazenas <salazenas@gmail.com>
2019-07-29 20:54:00 -0300

    unecessary div breaks the component css

--- frontend/src/components/tab/tabHeader.jsx
+++ frontend/src/components/tab/tabHeader.jsx
@@ -3,14 +3,12 @@ import React, { Component } from 'react'
 export class tabHeader extends Component {
     render() {
         return (
-            <div>
-                <li>
-                    <a href="javascript:;" data-toggle="tab" 
-                    data-target={this.props.target}>
-                        <i className={`fa fa-${this.props.icon}`}></i> {this.props.label}
-                    </a>
-                </li>
-            </div>
+            <li>
+                <a href="javascript:;" data-toggle="tab" 
+                data-target={this.props.target}>
+                    <i className={`fa fa-${this.props.icon}`}></i> {this.props.label}
+                </a>
+            </li>
         )
     }
 }
