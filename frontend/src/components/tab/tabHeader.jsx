3c1c7333bf8ed0ac1dbac83651d03476b8e12e84
salazenas <salazenas@gmail.com>
2019-07-29 23:53:38 -0300

    tab header with redux

--- frontend/src/components/tab/tabHeader.jsx
+++ frontend/src/components/tab/tabHeader.jsx
@@ -1,11 +1,18 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { selectTab } from './tabActions';
 
-export class tabHeader extends Component {
+export class TabHeader extends Component {
+
     render() {
+        const selected = this.props.tab.selected === this.props.target
+
         return (
-            <li>
-                <a href="javascript:;" data-toggle="tab" 
-                data-target={this.props.target}>
+            <li className={ selected ? 'active' : ''}>
+                <a href="javascript:;" data-toggle="tab"
+                     data-target={this.props.target}
+                    onClick={() => this.props.selectTab(this.props.target)}>
                     <i className={`fa fa-${this.props.icon}`}></i> {this.props.label}
                 </a>
             </li>
@@ -13,4 +20,10 @@ export class tabHeader extends Component {
     }
 }
 
-export default tabHeader
+const mapStateToProps = state => ({
+    tab: state.tab
+})
+
+const mapDispatchToProps = dispatch => bindActionCreators({ selectTab }, dispatch)
+
+export default connect(mapStateToProps, mapDispatchToProps)(TabHeader)
