bd32aefcc6c533875eff78e75137b51e87155e70
salazenas <salazenas@gmail.com>
2019-08-01 13:20:38 -0300

    tab content template

--- frontend/src/components/billingCycle/billingCycle.jsx
+++ frontend/src/components/billingCycle/billingCycle.jsx
@@ -1,12 +1,22 @@
 import React, { Component } from 'react'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+
 import ContentHeader from '../content/contentHeader'
 import Content from '../content/content'
 import Tabs from '../tab/tabs';
 import TabsContent from '../tab/tabsContent';
 import TabsHeader from '../tab/tabsHeader';
 import TabHeader from '../tab/tabHeader';
+import TabContent from '../tab/tabContent';
+import { selectTab } from '../tab/tabActions';
 
-export default class billingCycle extends Component {
+class BillingCycle extends Component {
+
+    componentWillMount() {
+        this.props.selectTab('tabList')
+    }
+
     render() {
         return (
             <div>
@@ -20,7 +30,18 @@ export default class billingCycle extends Component {
                             <TabHeader label="Excluir" icon="trash-o" target="tabDelete"></TabHeader>
                         </TabsHeader>
                         <TabsContent>
-
+                            <TabContent id="tabList">
+                                <h1>Lista</h1>
+                            </TabContent>
+                            <TabContent id="tabCreate">
+                                <h1>Incluir</h1>
+                            </TabContent>
+                            <TabContent id="tabUpdate">
+                                <h1>Alterar</h1>
+                            </TabContent>
+                            <TabContent id="tabDelete">
+                                <h1>Excluir</h1>
+                            </TabContent>
                         </TabsContent>
                     </Tabs>
                 </Content>
@@ -28,3 +49,9 @@ export default class billingCycle extends Component {
         )
     }
 }
+
+const mapDispatchToProps = dispatch => bindActionCreators({
+    selectTab
+}, dispatch)
+
+export default connect(null, mapDispatchToProps)(BillingCycle)
