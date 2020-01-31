4b56d90124a0e8e3fba7626a5c45631632a8122b
salazenas <salazenas@gmail.com>
2020-01-30 22:40:58 -0300

    authencation backend

--- backend/src/config/routes.js
+++ backend/src/config/routes.js
@@ -1,12 +1,29 @@
-const express = require('express')
+const express = require("express");
+const auth = require("./auth");
 
 module.exports = server => {
-    
-    // Base URL
-    const router = express.Router()
-    server.use('/api', router)
+  /*
+   * Rotas protegidas por Token JWT
+   */
+  const protectedApi = express.Router();
 
-    //Rotas de Ciclo de Pagamento
-    const billingCycle = require('../api/billingCycle/service')
-    billingCycle.register(router, '/billingCycles')
-}
+  server.use("/api", protectedApi);
+  protectedApi.use(auth);
+
+  const BillingCycle = require("../api/billingCycle/service");
+
+  BillingCycle.register(protectedApi, "/billingCycles");
+
+  /*
+   * Rotas abertas
+   */
+  const openApi = express.Router();
+
+  server.use("/oapi", openApi);
+
+  const AuthService = require("../api/user/authService");
+
+  openApi.post("/login", AuthService.login);
+  openApi.post("/signup", AuthService.signup);
+  openApi.post("/validateToken", AuthService.validateToken);
+};
